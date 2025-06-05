6586ccfa2b5cb1d439af0cccd9bb54dfb577273c
MUSE SHIRE <museshire@MUSEs-Mac-mini.local>
2025-06-05 22:49:50 +0200

    Cleaned up nested repo and added portfolio files

---
+++ portfolio/.eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/css");
+  
+  // Watch for CSS changes
+  eleventyConfig.addWatchTarget("./src/css/");
+
+  // Set input and output directories
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+}; 
